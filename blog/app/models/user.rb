4e7c3cc0bd9ef4471689eec26d868cdfcd38a867
HomieTrainee <danielalexpg@gmail.com>
2019-12-16 11:39:48 -0600

    Created blog project

---
+++ blog/app/models/user.rb
@@ -0,0 +1,8 @@
+class User
+  include Mongoid::Document
+  field :email, type: String
+  field :name, type: String
+  field :last_name, type: String
+  field :mobile_phone, type: String
+  field :work_place, type: String
+end
