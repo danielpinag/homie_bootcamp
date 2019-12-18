46dca971745d5fc23143e0d59ae354d37496e58b
HomieTrainee <danielalexpg@gmail.com>
2019-12-17 18:14:01 -0600

    Created Score Report Info

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello_world', to: 'hello_world#index'
+  get '/reports/info', to: 'reports#info'
 end
